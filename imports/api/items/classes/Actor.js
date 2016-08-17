969f8c18aa46126e64dde13de26edacafb738a82
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-08-17 21:46:32 +0300

    Take Actor from db, add hooks for events

--- imports/api/items/classes/Actor.js
+++ imports/api/items/classes/Actor.js
@@ -6,6 +6,7 @@ import '../../../utils/connectable.js';
 
 class Actor extends Item {
   setter(doc) {
+    super.setter(doc);
     let { parameters, pointList } = doc,
       update = 0;
 
@@ -30,8 +31,32 @@ class Actor extends Item {
   }
 
   draw(parent) {
-    this._svg = this.getSvg(parent).puppet(this._pointList[0], this._pointList[1], this);
+    let { _pointList, onDragStart, onDragMove, onDragEnd } = this;
+    this._svg = this.getSvg(parent).puppet(_pointList[0], _pointList[1], this, {
+      //onDragStart,
+      //onDragMove,
+      onDragEnd: (e, delta, type) => { this.onDragEnd(e, delta, type); }
+    });
     return this;
+  }
+
+  onDragEnd(e, delta, type) {
+    if(!type)
+      return;
+    console.log('onDragEnd', type);
+    if(type == 'Actor') {
+      this._pointList = [ this._pointList[0] + delta.movedX, this._pointList[1] + delta.movedY ]
+      this.update({modifier: {
+        _pointList: JSON.stringify(this._pointList)
+      }});
+      return;
+    }
+
+    let index = this._svg.jointInd(type);
+    this.position[index] = [ this.position[index][0] + delta.movedX, this.position[index][1] + delta.movedY ];
+    this.update({modifier: {
+      'parameters.position': JSON.stringify(this.position) 
+    }});
   }
 }
 
@@ -47,16 +72,23 @@ SVG.Puppet = SVG.invent({
   // Add custom methods to invented shape
   extend: {
     // Create method to proportionally scale the rounded corners
-    ini: function(x, y, { position, head, bone, joint }) { 
+    ini: function(x, y, { 
+      position, head, bone, joint 
+    }, {
+      onDragStart, onDragMove, onDragEnd
+    }) { 
       //this.item = svg.group();
-      var self = this 
+      let self = this 
       this.x = x;
       this.y = y;
       this.position = position;
       this.head = head;
       this.bone = bone;
       this.joint = joint;
-      
+      this.onDragStart = onDragStart || (() => {});
+      this.onDragMove = onDragMove || (() => {});
+      this.onDragEnd = onDragEnd || (() => {});
+
       this.scale=1;
       this.jointsItem = this.group()
       this.joints = [];
@@ -64,53 +96,83 @@ SVG.Puppet = SVG.invent({
       this.bonesItem = this.group()
       
       if (this.position !== undefined){
-          self.joints.push(this.circle(this.joint[0][1]*this.scale).cx((this.position[0][0]+x)*this.scale).cy((this.position[0][1]+y)*this.scale).attr({ 
+          self.joints.push(this.circle(this.joint[0][1]*this.scale*2).cx((this.position[0][0]+x)*this.scale).cy((this.position[0][1]+y)*this.scale).attr({ 
               stroke: "#fff",
               "stroke-width": 2
           }).draggy());
-          this.joints[0].on("dragmove", function(delta, event){
-              self.get()
-              //self.set(delta.x,delta.y)
-          })
+          let start, ini;
+          self.Actor = this.joints[0]
+            .on("dragstart", function(e){
+              let { pageX, pageY } = e.detail.event;
+              start = [ pageX, pageY ];
+              ini = [ pageX, pageY ];
+              self.onDragStart(e.detail.event, 'Actor');
+            })
+            .on("dragmove", function(e){
+              let { pageX, pageY } = e.detail.event;
+              self.move(pageX - ini[0], pageY - ini[1]);
+              self.onDragMove(e.detail.event, {
+                movedX: pageX - start[0],
+                movedY: pageY - start[1],
+                deltaX: pageX - ini[0],
+                deltaY: pageY - ini[1],
+              }, 'Actor');
+              ini = [ pageX, pageY ];
+            })
+            .on("dragend", function(e){
+              let { pageX, pageY } = e.detail.event;
+              self.move(pageX - ini[0], pageY - ini[1]);
+              console.log(e)
+              onDragEnd(e.detail.event, {
+                movedX: pageX - start[0],
+                movedY: pageY - start[1],
+                deltaX: pageX - ini[0],
+                deltaY: pageY - ini[1],
+              }, 'Actor');
+            });
       } else {
           //self.joints[0].cx(x).cy(y);
       }
        
         
-      for (var ndx=1;ndx <this.joint.length;ndx++){
-          //console.log(h.Class[position][ndx][1]) 
-          if (this.position !== undefined){
-              this.joints[ndx] = this.circle(this.joint[ndx][1]*this.scale)
-                .cx(this.position[ndx][0]*this.scale+x)
-                .cy(this.position[ndx][1]*this.scale+y)
-                .attr({
-              stroke: "#fff",
-              "stroke-width": 2*this.scale
-              }).draggy(
-                  /*
-                  function (x, y) {
-                  var res = {};
-                  var snapRange = 2;
-                  res.x = x - (x % snapRange);
-                  res.y = y + (y % snapRange);
-                  return res;
-              } */ 
-              ); 
-              
-          if (ndx == 3 || ndx ==4){
-              self.joints[ndx].on("dragend", function(e){
-              //console.log(e)
-              self.modHead(self) // self this
-                  
-          })
-          }    
-              
-              
-          
-          } else {
-              self.joints[ndx].cx(self.joints[ndx].cx()+x).cy(this.joints[ndx].cy()+y)
-              //console.log(self.joints[ndx].cy()+y)
-          }
+      for (let ndx=0;ndx <this.joint.length;ndx++){
+        //console.log(h.Class[position][ndx][1]) 
+        if (this.position !== undefined){
+            this.joints[ndx] = this.circle(this.joint[ndx][1]*this.scale)
+              .cx(this.position[ndx][0]*this.scale+x)
+              .cy(this.position[ndx][1]*this.scale+y)
+              .attr({
+                stroke: "#fff",
+                "stroke-width": 2*this.scale
+                })
+              .draggy(
+                /*
+                function (x, y) {
+                let res = {};
+                let snapRange = 2;
+                res.x = x - (x % snapRange);
+                res.y = y + (y % snapRange);
+                return res;
+              }*/)
+              .on("dragstart", function(e){
+                self.onDragStart(e.detail.event, self.joint[ndx][0]);
+              })
+              .on("dragmove", function(e){
+                let { pageX, pageY } = e.detail.event;
+                self.onDragMove(e.detail.event, self.joint[ndx][0]);
+                if(self.joint[ndx][0] == 'Base')
+                  self.Actor.cx(this.cx()).cy(this.cy());
+              })
+              .on("dragend", function(e){
+                if(self.joint[ndx][0] == 'Head' || self.joint[ndx][0] == 'Neck')
+                  self.modHead();
+                self.onDragEnd(e.detail.event, self.joint[ndx][0]);
+              });
+            
+        } else {
+            self.joints[ndx].cx(self.joints[ndx].cx()+x).cy(this.joints[ndx].cy()+y)
+            //console.log(self.joints[ndx].cy()+y)
+        }
 
       } 
       
@@ -119,18 +181,39 @@ SVG.Puppet = SVG.invent({
       if(this.head) {
           this.headItem = this.image(this.head, 100, 100) //.cx(this.joints[4].cx()).cy(this.joints[4].cy()+10)
           
-          this.modHead()
+          this.modHead();
           this.headItem.on("mouseover",function(e){
-              //alert(this);
               this.back()
           })
           this.headItem.on("mouseout",function(e){
-              //alert(this);
               this.front()
           })
       }
       }
       return this;
+    },
+    move: function(dx, dy) {
+      // Move all joints
+      this.joints.forEach(j => {
+        j.dmove(dx, dy);
+      });
+      // Update bones (connections)
+      this.bones.forEach(b => {
+        b.update();
+      })
+      // Update head
+      if(this.head)
+        this.modHead();
+    },
+    jointInd(name) {
+      let index;
+      this.joint.some((j,i) => {
+        if(j[0] == name) {
+          index = i;
+          return true;
+        }
+        return false;
+      });
     },
     modHead: function(self1){ 
       let neckJ, headJ;
@@ -140,12 +223,12 @@ SVG.Puppet = SVG.invent({
         if(j[0] == 'Head')
           headJ = i;
       });
-      var x = this.joints[headJ].cx()-this.joints[neckJ].cx();
-      var y = this.joints[headJ].cy()-this.joints[neckJ].cy();
-      var w = Math.sqrt(y*y + x*x);
+      let x = this.joints[headJ].cx()-this.joints[neckJ].cx();
+      let y = this.joints[headJ].cy()-this.joints[neckJ].cy();
+      let w = Math.sqrt(y*y + x*x);
       //console.log(w);
-      var ang = y/x;
-      var s = 1;
+      let ang = y/x;
+      let s = 1;
       if (y<0) s=-1;
       //console.log(ang);
       //self1.headItem.attr({width:2*w,height:2*w}).cx(self1.joints[4].cx()).cy(self1.joints[4].cy()+10); 
@@ -156,20 +239,21 @@ SVG.Puppet = SVG.invent({
         
     },
     setBones: function(){
-      for (var ndx in this.bone){
+      for (let ndx in this.bone){
           //console.log(this.joints[h.Class.bone[ndx][1]].connectable()) 
            
-          var conn = this.joints[this.bone[ndx][0]+1].connectable({
+          this.bones.push(
+            this.joints[this.bone[ndx][0]+1].connectable({
               container: this.bonesItem,
               width: this.bone[ndx][2]*this.scale
-          }, this.joints[this.bone[ndx][1]+1]
-          );
+            }, this.joints[this.bone[ndx][1]+1]
+          ));
       }
     },
     mirror: function(){
-      var x= this.joints[0].cx();
-      var y= this.joints[0].cy(), temp;
-      for (var ndx=1;ndx <this.joint.length;ndx++){
+      let x= this.joints[0].cx();
+      let y= this.joints[0].cy(), temp;
+      for (let ndx=1;ndx <this.joint.length;ndx++){
           
           this.joints[ndx].cx(-this.joints[ndx].cx()+2*x).cy(this.joints[ndx].cy())
       }
@@ -184,8 +268,8 @@ SVG.Puppet = SVG.invent({
           text = this.say;
       }
       //this.item.add( new SVG.TGraph)
-      var gr = this.jointsItem.group();
-      var dialog = gr.tgraph(0,0);
+      let gr = this.jointsItem.group();
+      let dialog = gr.tgraph(0,0);
       dialog.t("M"+(this.joints[4].cx()+50)+","+(this.joints[4].cy())+"<30F120>120F50A<90,20F130A<90,20F300A<90,20F130A<90,20F150Z");
       dialog.opacity=0.5; 
       dialog.render(false);
@@ -199,8 +283,8 @@ SVG.Puppet = SVG.invent({
       } else {
           text = this.think;
       }
-      var gr = this.jointsItem.group();
-      var dialog = gr.tgraph(0,0);
+      let gr = this.jointsItem.group();
+      let dialog = gr.tgraph(0,0);
       dialog.t("M"+(this.x+45)+","+(this.y-107)+">90A<180,7F15A<180,7F20ZM"+(this.x+70)+","+(this.y-120)+"A<180,15F50A<180,15F50ZM"+(this.x+50)+","+(this.y-150)+"A<180,75F250A<180,75Z");
       dialog.opacity=0.5
       dialog.render(false);
@@ -213,12 +297,12 @@ SVG.Puppet = SVG.invent({
   construct: {
     // Create a rounded element
     
-    puppet: function(x,y,obj) {
+    puppet: function(x,y,obj,callb) {
         //this.put(new SVG.Puppet)
         //console.log("om") 
         // new SVG.Puppet
         //this.ini(x,y,stance)
-      return this.put(new SVG.Puppet).ini(x,y,obj);
+      return this.put(new SVG.Puppet).ini(x,y,obj,callb);
     } 
 
   }
